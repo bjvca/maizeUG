d57ea2a1296a7fe7223c64222ce832deee0c9073
bjvca <bjorn.vancampenhout@gmail.com>
2017-05-01 14:52:57 +0200

    power analysis for different sample size per treatment now works in this example with three treatments

--- pre-analysis plan/power_test.R
+++ pre-analysis plan/power_test.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-
+system("rm myfile")
 library(foreign)
 library(foreach)
 library(doParallel)
@@ -7,28 +7,30 @@ registerDoParallel(cores=4)
 
 alpha <- .05
 N <- 3000
-sims <- 100
-minsamp <- 100
+sims <- 200
+minsamp <- 400
+bystep <- 50
 
 ptm <- proc.time()
 ### grid search
 ## first fix control at 1/3 of N
  
-possible.n2 <- seq(minsamp,floor(N-minsamp), by=100)
+possible.n2 <- seq(minsamp,N-3*minsamp, by=bystep)
+
 #power.fullranking <- array(NA, c(length(possible.n2), length(possible.n2), length(possible.n2)))
 power.fullranking <- array(NA, c(length(possible.n2), length(possible.n2)))
 
-res <- foreach (j = 1:(length(possible.n2)-3), .combine=c) %dopar% {
+res <- foreach (j = 1:(length(possible.n2)) ) %dopar% {
 #for (j in 1:(length(possible.n2)-3))  {
 
 #r <- foreach (t = 1:(length(possible.n2)-j -2)) %dopar% {
-for (t in 1:(length(possible.n2)-j - 2) ) {
-#for (u in 1:(length(possible.n2)-j-t-1) ) {
+for (t in 1:(length(possible.n2) - (j-1)) ) {
+for (u in 1:(length(possible.n2) - (t -1) - (j - 1)) ) {
 
 g1 <- possible.n2[j]
 g2 <- possible.n2[t]
-#g3 <- possible.n2[u]
-Z.sim <- sample(as.factor(c(rep("T3",floor(N-g1-g2)),  rep("T2", g2), rep("T1", g1))))
+g3 <- possible.n2[u]
+Z.sim <- sample(as.factor(c(rep("T4",floor(N-g1-g2-g3)), rep("T3", g3), rep("T2", g2), rep("T1", g1))))
 
 
 
@@ -41,17 +43,17 @@ Z.sim <- sample(as.factor(c(rep("T3",floor(N-g1-g2)),  rep("T2", g2), rep("T1", 
     Y0 <-  rnorm(n=length(Z.sim), mean=60, sd=20)
     tau_1 <- 1
     tau_2 <- 4
- #   tau_3 <- 5
+   tau_3 <- 5
     Y1 <- Y0 + tau_1
     Y2 <- Y0 + tau_2
-#Y3 <- Y0 + tau_3
+Y3 <- Y0 + tau_3
    # Z.sim <- complete_ra(N=N, num_arms=3)
-   # Y.sim <- Y0*(Z.sim=="T4") + Y3*(Z.sim=="T3") + Y1*(Z.sim=="T1") + Y2*(Z.sim=="T2")
-Y.sim <- Y0*(Z.sim=="T3") + Y1*(Z.sim=="T1") + Y2*(Z.sim=="T2")
+    Y.sim <- Y0*(Z.sim=="T4") + Y3*(Z.sim=="T3") + Y1*(Z.sim=="T1") + Y2*(Z.sim=="T2")
+#Y.sim <- Y0*(Z.sim=="T3") + Y1*(Z.sim=="T1") + Y2*(Z.sim=="T2")
     frame.sim <- data.frame(Y.sim, Z.sim)
     
-    fit.T2vsC.sim <- lm(Y.sim ~ Z.sim=="T3", data=frame.sim)
-    fit.T2vsT1.sim <- lm(Y.sim ~ Z.sim=="T2", data=subset(frame.sim, Z.sim!="T3"))
+    fit.T2vsC.sim <- lm(Y.sim ~ Z.sim=="T4", data=frame.sim)
+    fit.T2vsT1.sim <- lm(Y.sim ~ Z.sim=="T2", data=subset(frame.sim, Z.sim!="T3" &  Z.sim!="T4"))
     
 
 
@@ -60,13 +62,33 @@ Y.sim <- Y0*(Z.sim=="T3") + Y1*(Z.sim=="T1") + Y2*(Z.sim=="T2")
   }
 
 #power.fullranking[j,t,u] <- mean(p.T2vsC < alpha/2 & p.T2vsT1 < alpha/2)
-power.fullranking[j,t] <- mean(p.T2vsC < alpha/2 & p.T2vsT1 < alpha/2)
+#power.fullranking[j,t] <- mean(p.T2vsC < alpha/2 & p.T2vsT1 < alpha/2)
+write(c(j,t,u, mean(p.T2vsC < alpha/2 & p.T2vsT1 < alpha/2)),file="myfile",sep = ";",append=TRUE)
 
-#}
 }
+}
+#return(power.fullranking[j,])
+
 }
 
 proc.time() - ptm
-print()
- which(power.fullranking == max(power.fullranking, na.rm=T),arr.ind=T)
+ dta <- read.csv("/home/bjvca/data/projects/PASIC/riceRCT/endline/myfile", sep = ";", header = F)
+
+
+
+### to run this on amamzon EC
+ssh -i "bjornkey.pem" ubuntu@publicIPaddress
+
+sudo apt-get update
+sudo apt-get install r-base-core
+
+
+sudo R
+install.packages("foreach")
+install.packages("doParallel")
+
+##than copy in code
+
+
+
 
